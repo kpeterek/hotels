12b86d93e29325aa4d4ff7c3f878da349ed4ee3c
kpeterek <64157678+kpeterek@users.noreply.github.com>
2021-04-04 15:59:00 -0700

    Add files via upload

---
+++ streamlit_BOV.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  2 15:08:30 2021
+
+@author: KyleP
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck
+import os
+
+os.chdir(r"D:\KyleP\Coding\Work Scripts")
+
+import DodgeData as dd
+
+
+st.title('Explore Your Hotels!!!')
+
+
+
+st.write('''
+         
+         All the hotel data you can handle bro!''')
+         
+
+df = pd.read_csv(r"C:\Users\KyleP\Box\YB Hotels\__BOVs\2021\Snapshot of Current BOVs 2021-03-29.csv")
+dodge_pipeline = pd.read_pickle(r'C:\Users\KyleP\Box\YB Hotels\CBRE Hotels Pipeline Data\Dodge Data\pipeline.pkl')
+dodge_census = pd.read_pickle(r'C:\Users\KyleP\Box\YB Hotels\CBRE Hotels Pipeline Data\Dodge Data\census.pkl')
+
+states = dodge_pipeline.State.str.replace(" ","").dropna().unique()
+
+# markets = 1
+# brands= dodge_pipeline.Chain.dropna().unique()
+
+# stars = dodge_census.StarID.dropna().unique()
+
+# state = st.sidebar.selectbox('Select State',states)
+
+# market = st.sidebar.selectbox('Select Market',dodge_pipeline[dodge_pipeline.State == state]['Submarket'].dropna().unique())
+
+# brand = st.sidebar.selectbox('Select brand',brands)
+
+
+# st.write(state)
+# st.write(brand)
+
+data = dodge_pipeline[['Title','Address','City','State','PostalCode','Units','Target Open Date','Phase','Latitude','Longitude']]
+
+
+
+
+star = st.sidebar.text_input('Enter Star ID')
+filter_by = st.sidebar.selectbox('Filter by?', ['radius','tract','city'])
+radius = st.sidebar.text_input('Radius?')
+
+submit = st.sidebar.button('run new supply')
+if submit:
+    data = dd.newsupply(int(star),radius=int(radius),filter_by=filter_by)
+    
+    
+# st.write('run a radius sample',dd.newsupply(star))
+
+
+st.write(data.dropna())
+
+# button_clicked = st.sidebar.button("OK")
+
+data.rename(columns = {'Latitude':'lat','Longitude':'lon'},inplace=True)
+data.dropna(inplace=True)
+
+st.map(data)
+             
