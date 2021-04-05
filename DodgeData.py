247726a4a83cd19589c6aaaed6713a51f98af1fc
kpeterek <64157678+kpeterek@users.noreply.github.com>
2021-04-04 19:06:26 -0700

    Update DodgeData.py

--- DodgeData.py
+++ DodgeData.py
@@ -16,19 +16,6 @@ from geopy.distance import geodesic
 import matplotlib.pyplot as plt
 
 
-    
-pd.set_option('display.max_columns', 15,'max_colwidth',70,'display.width', 500, 'display.max_rows',100)
-pd.set_option('mode.sim_interactive', True)
-pd.set_option('expand_frame_repr', True)
-# os.getcwd()
-
-# os.listdir()
-cols_needed = ['Title','Address','City','State','PostalCode','Units','Target Open Date','Phase','Latitude','Longitude','distance','sort']
-cols_exist = ['StarID','Property','Address','City','State','postalcode','Rooms','OpenDate','Latitude','Longitude','distance']
-
-
-# dodge_pipeline = pd.read_pickle(r'C:\Users\KyleP\Box\YB Hotels\CBRE Hotels Pipeline Data\Dodge Data\pipeline.pkl')
-# dodge_census = pd.read_pickle(r'C:\Users\KyleP\Box\YB Hotels\CBRE Hotels Pipeline Data\Dodge Data\census.pkl')
 
 def update_dodge():
     os.chdir(r"C:\Users\KyleP\Box\YB Hotels\CBRE Hotels Pipeline Data\Dodge Data")
@@ -57,8 +44,8 @@ def get_dodge_data_census():
 def newsupply(STR,radius=7,filter_by = 'radius'):
     # STR = int(input('Enter the Subject Property STR Number?: '))
     # radius = 40.0
-    dodge_pipeline = pd.read_pickle(r'C:\Users\KyleP\Box\YB Hotels\CBRE Hotels Pipeline Data\Dodge Data\pipeline.pkl')
-    dodge_census = pd.read_pickle(r'C:\Users\KyleP\Box\YB Hotels\CBRE Hotels Pipeline Data\Dodge Data\census.pkl')
+    dodge_pipeline = pd.read_csv('pipeline.csv')
+    dodge_census = pd.read_csv('census.csv')
     phases = ['Underway','Planning','Final Planning']
     if (dodge_census[dodge_census['StarID'] == STR]).any(axis=None) == True:
         prop_name = dodge_census['Property'][dodge_census['StarID'] == STR].iloc[:,].item()
@@ -89,35 +76,10 @@ def newsupply(STR,radius=7,filter_by = 'radius'):
         new_supply.columns
         output_ns = new_supply[cols_needed]
         output_ns.sort_values('sort',ascending = True,inplace=True)
-        if output_ns.empty:
-            print('There are no incoming properties. Thank you')
-        else:
-            print('Mean: \n',new_supply[['Units','Phase']].groupby('Phase').mean(),'\n',"TOTAL                 ",int(new_supply['Units'].dropna().sum()),'\n\n')
-            print('Total: \n',new_supply[['Title','Phase']].groupby('Phase').count(),'\n',"AVERAGE               ",int(new_supply['Units'].isnull().count()),'\n\n')
-            print(output_ns.iloc[:,:-1],'\n\n')
-            time.sleep(1)
-            # output_ns.groupby(['Phase'],sort=False)['Units'].sum().astype('int').to_clipboard(sep=',',header = None,index= True)
-            # print('      ',output_text,'\n')
-            print('                Total Incoming Properties: ',new_supply['Units'].count(
-                ),'\n                Total Incoming Rooms: ',int(new_supply['Units'].sum()),'\n\n')
-            # root.bell()
-            time.sleep(1)
-        output_ns.to_clipboard(sep=",",header = False,index =False)
-        print(f'{prop_name} new supply is now in your clipboard, {output_text}')
-        # return output_ns
-    else:
-        print('Property not found')
-        pass
     return output_ns
         
 
 
-
-# for state in pd.Series(dodge_pipeline['State'].unique()):
-#     print(pd.Series(dodge_pipeline['Market'][dodge_pipeline.query('State == @state')]))
-
-
-# 
 
 
 def str_comps():
@@ -138,64 +100,6 @@ def str_city():
     df = dodge_census[dodge_census['City'] == dodge_census['City'][dodge_census['StarID'] == prop].iloc[:,].item()]  
     df[cols_exist].to_clipboard(sep=",",header = True,index =False)      
     
-    
-# df = dodge_census[dodge_census['City'] == dodge_census['City'][dodge_census['StarID'] == 7570].iloc[:,].item()]      
-    
-# for i in dodge_census.columns[:8]:
-#     print(i)
-    
-# def newsupply(STR,radius):
-#     # STR = int(input('Enter the Subject Property STR Number?: '))
-#     # radius = 3.0
-#     phases = ['Underway','Planning','Final Planning']
-#     prop_name = dodge_census['Property'][dodge_census['StarID'] == STR].iloc[:,].item()
-#     prop_city = dodge_census['City'][dodge_census['StarID'] == STR].iloc[:,].item()
-#     coords_subj = str(dodge_census['Latitude'][dodge_census['StarID'] == STR].iloc[:,].item()),str(dodge_census['Longitude'][dodge_census['StarID'] == STR].iloc[:,].item())
-#     phase_sort = {phases[0]:0,phases[1]:2,phases[2]:1}
-#     # SubTract = existing_str['Tract'].loc[existing_str.index == STR].iloc[:,].item()
-#     # SubMkt = existing_str['Market'].loc[existing_str.index  == STR].iloc[:,].item()
-#     new_supply = pipeline_str.loc[(pipeline_str['Project Phase'].isin(list(phases)))]
-#     new_supply['Lat,Lon'] = list(zip(new_supply['Latitude'],new_supply['Longitude']))
-#     new_supply['distance'] = [geodesic(coords_subj,new_supply['Lat,Lon'].loc[new_supply.index == x]).miles for x in new_supply.index]
-#     filter_input = input("Filter by ('radius','city','market','tract'): ")
-#     if filter_input == 'radius':
-#         new_supply.query('distance <= @radius',inplace= True)
-#         output_text = str(f'Within a {radius} mile radius of {prop_name}:')
-#     elif filter_input == 'city':
-#         new_supply = new_supply.query('City == @prop_city & Tract == @SubTract')
-#         output_text = str(f"Based on {prop_city}'s (city) pipeline of {prop_name}: ")
-#     elif filter_input == 'market':
-#         new_supply = new_supply.query('Market == @SubMkt')
-#         output_text = str(f'Based on the {SubMkt} (market) pipeline of {prop_name}:')
-#     elif filter_input == 'tract':    
-#         new_supply = new_supply.query('Tract == @SubTract')
-#         output_text = str(f'Based on the {SubTract} pipeline of {prop_name}:')
-#     new_supply['sort'] = new_supply['Project Phase'].map(phase_sort)
-#     output_ns = new_supply[cols_needed]
-#     output_ns.sort_values('sort',ascending = True,inplace=True)
-#     if output_ns.empty:
-#         print('There are no incoming properties. Thank you')
-#     else:
-#         print('Mean: \n',new_supply[['Room Count','Project Phase']].groupby('Project Phase').mean(),'\n',"TOTAL                 ",int(new_supply['Room Count'].dropna().sum()),'\n\n')
-#         print('Total: \n',new_supply[['Room Count','Project Phase']].groupby('Project Phase').count(),'\n',"AVERAGE               ",int(new_supply['Room Count'].isnull().mean()),'\n\n')
-#         print(output_ns.iloc[:,:-1],'\n\n')
-#         time.sleep(1)
-#         output_ns.groupby(['Project Phase'],sort=False)['Room Count'].sum().astype('int').to_clipboard(sep=',',header = None,index= True)
-#         print('      ',output_text,'\n')
-#         print('                Total Incoming Properties: ',new_supply['Room Count'].count(
-#             ),'\n                Total Incoming Rooms: ',int(new_supply['Room Count'].sum()),'\n\n')
-#         # root.bell()
-#         time.sleep(1)
-    
-#         for phase in phases:
-#             data_dump = new_supply.loc[(pipeline_str['Project Phase'] == phase)].iloc[:,np.r_[0,-6,-5]]
-#             data_dump.to_clipboard(sep= ",",index = False,header = None)
-#             print(phase,'\n',data_dump.head(10),'\n\n')
-#             # root.bell()
-#             time.sleep(1)
-#         output_ns.iloc[:,0:-2].to_clipboard(sep = ",", header = None,index = False)
-
-
     
 
 def str_search():
