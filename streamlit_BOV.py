baf78a85b0f9715e6c70925b923c750dc9ca80e3
kpeterek <64157678+kpeterek@users.noreply.github.com>
2021-04-04 18:42:09 -0700

    Update streamlit_BOV.py

--- streamlit_BOV.py
+++ streamlit_BOV.py
@@ -18,6 +18,54 @@ cols_needed = ['Title','Address','City','State','PostalCode','Units','Target Ope
 cols_exist = ['StarID','Property','Address','City','State','postalcode','Rooms','OpenDate','Latitude','Longitude','distance']
 
 
+st.title('Explore Your Hotels!!!')
+
+
+
+st.write('''
+         
+         All the hotel data you can handle bro!''')
+
+states = dodge_pipeline.State.str.replace(" ","").dropna().unique()
+
+# markets = 1
+# brands= dodge_pipeline.Chain.dropna().unique()
+
+# stars = dodge_census.StarID.dropna().unique()
+
+# state = st.sidebar.selectbox('Select State',states)
+
+# market = st.sidebar.selectbox('Select Market',dodge_pipeline[dodge_pipeline.State == state]['Submarket'].dropna().unique())
+
+# brand = st.sidebar.selectbox('Select brand',brands)
+
+
+# st.write(state)
+# st.write(brand)
+
+data = dodge_pipeline[['Title','Address','City','State','PostalCode','Units','Target Open Date','Phase','Latitude','Longitude']]
+
+star = st.sidebar.text_input('Enter Star ID')
+filter_by = st.sidebar.selectbox('Filter by?', ['radius','tract','city'])
+radius = st.sidebar.text_input('Radius?')
+
+submit = st.sidebar.button('run new supply')
+if submit:
+    data = newsupply(int(star),float(radius),filter_by)
+    
+
+# st.write('run a radius sample',dd.newsupply(star))
+
+
+st.write(data.dropna())
+
+# button_clicked = st.sidebar.button("OK")
+
+data.rename(columns = {'Latitude':'lat','Longitude':'lon'},inplace=True)
+data.dropna(inplace=True)
+
+st.map(data)
+             
 def newsupply(STR,radius=7,filter_by = 'radius'):
     # STR = int(input('Enter the Subject Property STR Number?: '))
     # radius = 40.0
@@ -53,19 +101,6 @@ def newsupply(STR,radius=7,filter_by = 'radius'):
         new_supply.columns
         output_ns = new_supply[cols_needed]
         output_ns.sort_values('sort',ascending = True,inplace=True)
-        if output_ns.empty:
-            print('There are no incoming properties. Thank you')
-        else:
-            print('Mean: \n',new_supply[['Units','Phase']].groupby('Phase').mean(),'\n',"TOTAL                 ",int(new_supply['Units'].dropna().sum()),'\n\n')
-            print('Total: \n',new_supply[['Title','Phase']].groupby('Phase').count(),'\n',"AVERAGE               ",int(new_supply['Units'].isnull().count()),'\n\n')
-            print(output_ns.iloc[:,:-1],'\n\n')
-            time.sleep(1)
-            # output_ns.groupby(['Phase'],sort=False)['Units'].sum().astype('int').to_clipboard(sep=',',header = None,index= True)
-            # print('      ',output_text,'\n')
-            print('                Total Incoming Properties: ',new_supply['Units'].count(
-                ),'\n                Total Incoming Rooms: ',int(new_supply['Units'].sum()),'\n\n')
-            # root.bell()
-            time.sleep(1)
         output_ns.to_clipboard(sep=",",header = False,index =False)
         print(f'{prop_name} new supply is now in your clipboard, {output_text}')
         # return output_ns
@@ -73,60 +108,3 @@ def newsupply(STR,radius=7,filter_by = 'radius'):
         print('Property not found')
         pass
     return output_ns
-
-st.title('Explore Your Hotels!!!')
-
-
-
-st.write('''
-         
-         All the hotel data you can handle bro!''')
-         
-
-
-dodge_pipeline = pd.read_csv('pipeline.csv')
-dodge_census = pd.read_csv('census.csv')
-
-states = dodge_pipeline.State.str.replace(" ","").dropna().unique()
-
-# markets = 1
-# brands= dodge_pipeline.Chain.dropna().unique()
-
-# stars = dodge_census.StarID.dropna().unique()
-
-# state = st.sidebar.selectbox('Select State',states)
-
-# market = st.sidebar.selectbox('Select Market',dodge_pipeline[dodge_pipeline.State == state]['Submarket'].dropna().unique())
-
-# brand = st.sidebar.selectbox('Select brand',brands)
-
-
-# st.write(state)
-# st.write(brand)
-
-data = dodge_pipeline[['Title','Address','City','State','PostalCode','Units','Target Open Date','Phase','Latitude','Longitude']]
-
-
-
-
-star = st.sidebar.text_input('Enter Star ID')
-filter_by = st.sidebar.selectbox('Filter by?', ['radius','tract','city'])
-radius = st.sidebar.text_input('Radius?')
-
-submit = st.sidebar.button('run new supply')
-if submit:
-    data = newsupply(star,radius,filter_by)
-    
-    
-# st.write('run a radius sample',dd.newsupply(star))
-
-
-st.write(data.dropna())
-
-# button_clicked = st.sidebar.button("OK")
-
-data.rename(columns = {'Latitude':'lat','Longitude':'lon'},inplace=True)
-data.dropna(inplace=True)
-
-st.map(data)
-             
