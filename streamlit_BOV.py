5eb4d560e233638ab892d17d96d4f983c0cfaebc
kpeterek <64157678+kpeterek@users.noreply.github.com>
2021-06-02 11:14:22 -0700

    Update streamlit_BOV.py

--- streamlit_BOV.py
+++ streamlit_BOV.py
@@ -153,5 +153,3 @@ def star_data_input():
     star_df.columns = ['OCC_my_prop','OCC_comp','OCC_Index','OCC_Rank', 'OCC_per_chg_my_prop','OCC_per_chg_comp','OCC_per_chg_index','OCC_per_chg_rank','ADR_my_prop','ADR_comp','ADR_Index','ADR_Rank','ADR_per_chg_my_prop','ADR_per_chg_comp','ADR_per_chg_index','ADR_per_chg_rank','RevPAR_my_prop','RevPAR_comp','RevPAR_Index','RevPAR_Rank','RevPAR_per_chg_my_prop','RevPAR_per_chg_comp','RevPAR_per_chg_index','RevPAR_per_chg_rank','STARID']
     star_df.drop_duplicates(subset=['Date','STARID'],inplace=True)
     return star_df , comp_set
-         
-st.write(star_df)
