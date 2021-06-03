1639d0b756d7efaef85716d23d8a785b163a8db2
kpeterek <64157678+kpeterek@users.noreply.github.com>
2021-06-03 11:16:48 -0700

    Update streamlit_BOV.py

--- streamlit_BOV.py
+++ streamlit_BOV.py
@@ -94,8 +94,8 @@ href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click
 st.markdown(href, unsafe_allow_html=True)
 
                   
-def star_data_input(files):
-    cols = [0,1,2,3,5,6,7,8,12,13,14,15,17,18,19,20,24,25,26,27,29,30,31,32,34,35]
+def star_data_input_zip(files):
+    cols = [0,1,2,3,5,6,7,8,12,13,14,15,17,18,19,20,24,25,26,27,29,30,31,32,34]
     star_df = pd.DataFrame()
     comp_set = pd.DataFrame()
     archive = zipfile.ZipFile(files, 'r')
@@ -105,30 +105,32 @@ def star_data_input(files):
             if file.endswith('.xlsx'):
                 print(file)
                 try:
-                    date = pd.to_datetime(pd.read_excel(xlfile,sheet_name='Glance',skiprows=4).iloc[0,1],
-                                          infer_datetime_format=True)+MonthEnd(1)
-                    comps = pd.read_excel(xlfile,sheet_name='Response',skiprows=21,usecols='C:L',header=0).dropna(axis=0,how = 'all').dropna(axis=1,how='all').dropna(subset=['Name'])
-                    star = pd.read_excel(xlfile,sheet_name='Comp',skiprows=18,usecols='B:T',nrows=34,header=1,
-                                     index_col=0,parse_dates=True).T
-                    star['date'] = pd.date_range(end=date,periods = 18,freq='M')
-                    sub_prop = comps.iloc[0,0]
-                    star['STARID'] = sub_prop
-                    star_df = star_df.append(star.reset_index().drop(columns=['index']))
-                    comps['Subj_prop'] = sub_prop
-                    comp_set = comp_set.append(comps).drop_duplicates().sort_index(ascending=True)
-                except:
+                    if 'Monthly STAR Report' in pd.read_excel(xlfile,sheet_name='Table of Contents',skiprows=2,usecols='B',header=0,nrows=0).columns[0]:
+                        date = pd.to_datetime(pd.read_excel(xlfile,sheet_name='Glance',skiprows=4).iloc[0,1],
+                                              infer_datetime_format=True)+MonthEnd(1)
+                        comps = pd.read_excel(xlfile,sheet_name='Response',skiprows=21,usecols='C:L',header=0).dropna(axis=0,how = 'all').dropna(axis=1,how='all').dropna(subset=['Name'])
+                        star = pd.read_excel(xlfile,sheet_name='Comp',skiprows=18,usecols='B:T',nrows=34,header=1,
+                                         index_col=0,parse_dates=True).T
+                        star.index = pd.date_range(end=date,periods = 18,freq='M')
+                        sub_prop = comps.iloc[0,0]
+                        star['STARID'] = sub_prop
+                        star_df = star_df.append(star).drop_duplicates().sort_index(ascending=True)
+                        comps['Subj_prop'] = sub_prop
+                        comp_set = comp_set.append(comps).drop_duplicates().sort_index(ascending=True)
+                except ValueError:
                     pass
-        # star_df = star_df.append(star).drop_duplicates().sort_index(ascending=True)
-        # comp_set = comp_set.append(comps).drop_duplicates().sort_index(ascending=True)
-        # star_df['StarID'] = sub_prop
-        # star_df.columns = ['RevPAR_my_prop','RevPAR_comp','ADR_my_prop','ADR_comp','OCC_my_prop','OCC_comp','StarID']
-        star_df = star_df.iloc[:,cols]
-        star_df.iloc[:,4:6] = star_df.iloc[:,4:6]/100
-        star_df.to_clipboard(header=False)
-        star_df.columns = ['OCC_my_prop','OCC_comp','OCC_Index','OCC_Rank', 'OCC_per_chg_my_prop','OCC_per_chg_comp','OCC_per_chg_index','OCC_per_chg_rank','ADR_my_prop','ADR_comp','ADR_Index','ADR_Rank','ADR_per_chg_my_prop','ADR_per_chg_comp','ADR_per_chg_index','ADR_per_chg_rank','RevPAR_my_prop','RevPAR_comp','RevPAR_Index','RevPAR_Rank','RevPAR_per_chg_my_prop','RevPAR_per_chg_comp','RevPAR_per_chg_index','RevPAR_per_chg_rank','date','STARID']
-        star_df.drop_duplicates(subset=['date','STARID'],inplace=True)
-        del star_df['date']
-        return star_df
+    print(star_df.info())
+    star_df = star_df.iloc[:,cols]
+    # star_df['StarID'] = sub_prop
+    # star_df.columns = ['RevPAR_my_prop','RevPAR_comp','ADR_my_prop','ADR_comp','OCC_my_prop','OCC_comp','StarID']
+    star_df.iloc[:,4:6] = star_df.iloc[:,4:6]/100
+    star_df=star_df[~star_df.index.duplicated(keep='last')]
+    comp_set = comp_set[~comp_set['STR#'].duplicated(keep='last')]
+    star_df.to_clipboard(header=False)
+    star_df.columns = ['OCC_my_prop','OCC_comp','OCC_Index','OCC_Rank', 'OCC_per_chg_my_prop','OCC_per_chg_comp','OCC_per_chg_index','OCC_per_chg_rank','ADR_my_prop','ADR_comp','ADR_Index','ADR_Rank','ADR_per_chg_my_prop','ADR_per_chg_comp','ADR_per_chg_index','ADR_per_chg_rank','RevPAR_my_prop','RevPAR_comp','RevPAR_Index','RevPAR_Rank','RevPAR_per_chg_my_prop','RevPAR_per_chg_comp','RevPAR_per_chg_index','RevPAR_per_chg_rank','STARID']
+    # star_df.drop_duplicates(subset=['OCC_my_prop','ADR_my_prop','RevPAR_my_prop'],inplace=True)
+    print(comp_set)
+    return star_df
 
 
 def excel_file_merge(zip_file_name):
