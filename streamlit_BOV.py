7484e0ff3316895c737fe9d3b82005beeed17226
kpeterek <64157678+kpeterek@users.noreply.github.com>
2021-06-04 14:14:27 -0700

    Update streamlit_BOV.py

--- streamlit_BOV.py
+++ streamlit_BOV.py
@@ -32,6 +32,23 @@ import datetime as dt
 from bs4 import BeautifulSoup
 import requests
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def remote_css(url):
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
+
+def icon(icon_name):
+    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+
+local_css("style.css")
+remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+
+icon("search")
+selected = st.text_input("", "Search...")
+button_clicked = st.button("OK")
+
 def tsa_info(moving_avg = 7):
     url = 'https://www.tsa.gov/coronavirus/passenger-throughput'
     response = requests.get(url)
