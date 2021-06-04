6c5982faa26fb862a26c6ffb34c8b0da37960033
kpeterek <64157678+kpeterek@users.noreply.github.com>
2021-06-03 19:32:40 -0700

    Update streamlit_BOV.py

--- streamlit_BOV.py
+++ streamlit_BOV.py
@@ -226,7 +226,8 @@ csv = data.to_csv(index=False)
 b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
 href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
 st.markdown(href, unsafe_allow_html=True)
-open('Closings_pickle.pkl', 'rb') as f: closings = pickle.load(f)
+with open('Closings_pickle.pkl', 'rb') as f: 
+	closings = pickle.load(f)
 st.write(closings)
 # Main panel
 #if st.sidebar.button('Submit'):
