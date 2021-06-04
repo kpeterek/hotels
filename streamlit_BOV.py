cc76d00a51776103b1b497399a73c702a4e9b609
kpeterek <64157678+kpeterek@users.noreply.github.com>
2021-06-04 15:23:38 -0700

    Update streamlit_BOV.py

--- streamlit_BOV.py
+++ streamlit_BOV.py
@@ -208,8 +208,8 @@ st.write('''
          
          All the hotel data you can handle bro!''')
 mkts = kalibri_data['Submarket'].drop_duplicates()
-make_choice = st.sidebar.selectbox('Select your SUBarket:', mkts)
-st.write(kalibri_data[kalibri_data.Submarket == make_choice])
+make_choice = st.sidebar.selectbox('Select your Submarket:', mkts)
+st.write(kalibri_data[kalibri_data.Submarket.isin([make_choice])])
 #years = df["year"].loc[df["make"] = make_choice]
 #year_choice = st.sidebar.selectbox('', years) 
 
