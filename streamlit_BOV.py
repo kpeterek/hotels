04ff1ca105284b229c4e613e476532b477e4f936
kpeterek <64157678+kpeterek@users.noreply.github.com>
2021-06-05 00:19:59 -0700

    Update streamlit_BOV.py

--- streamlit_BOV.py
+++ streamlit_BOV.py
@@ -276,7 +276,7 @@ def main():
 			href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
 			st.markdown(href, unsafe_allow_html=True)
 	elif choice == 'Quick Deal Analysis':
-		@st.cache
+		#@st.cache
 		fp.fin_planning()
 		
 
