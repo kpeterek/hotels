e98ac900f7e81c8ef9b4ce5db28fae5966c320db
kpeterek <64157678+kpeterek@users.noreply.github.com>
2021-06-04 23:57:29 -0700

    Update fin_plan.py

--- fin_plan.py
+++ fin_plan.py
@@ -12,9 +12,9 @@ def fin_planning():
   colRooms, colRevPAR = st.beta_columns(2)
 
   with colRooms:
-      rooms = st.number_input("Enter the hotel room count: ", min_value=1, format='%d')
+      rooms = st.slider("Enter the hotel room count: ", min_value=1, max_value = 1000, step = 1)
   with colRevPAR:
-      rev_par = st.slider("Enter stabilized RevPAR: ", min_value=1, max_value = 1000, step = 1)
+      rev_par = st.slider("Enter stabilized RevPAR: ", min_value=20, max_value = 300, step = 1)
 
   with colExpenses1:
       st.subheader("Monthly Rental")
