50444c84b17e351d46d3b79ad96b2a3c35206e8b
kpeterek <64157678+kpeterek@users.noreply.github.com>
2022-01-24 15:59:47 -0800

    Update streamlit_BOV.py

--- streamlit_BOV.py
+++ streamlit_BOV.py
@@ -289,7 +289,10 @@ def main():
 			data = dd.str_lookup(keys)
 			st.write(data)
 			star = st.text_input('Enter Hotel Star')
-			st.write(str_find(star))
+			submit2 = st.sidebar.button('Retrieve Hotel')
+			if submit2:
+				hotel = str_find(star)
+				st.write(str_find(star))
 
 #years = df["year"].loc[df["make"] = make_choice]
 #year_choice = st.sidebar.selectbox('', years) 
