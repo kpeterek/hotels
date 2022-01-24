48406aa634ab5ba9cde1e6cf1814cfddc3a9d136
kpeterek <64157678+kpeterek@users.noreply.github.com>
2022-01-24 13:53:52 -0600

    Update streamlit_BOV.py

--- streamlit_BOV.py
+++ streamlit_BOV.py
@@ -289,14 +289,8 @@ def main():
 		submit = st.sidebar.button('Search Hotel')
 		if submit:
 			st.write(data.dropna())
-			data.rename(columns = {'Latitude':'lat','Longitude':'lon'},inplace=True)
-			data.dropna(inplace=True)
-			st.map(data)
-			st.header("File Download")
-			csv = data.to_csv(index=False)
-			b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-			href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
-			st.markdown(href, unsafe_allow_html=True)
+			
+
 
 #years = df["year"].loc[df["make"] = make_choice]
 #year_choice = st.sidebar.selectbox('', years) 
